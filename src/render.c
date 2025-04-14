b5f500d354a9da1e75b77044edd56aa0ae0afc57
royeldar <royeldar@hotmail.com>
2025-04-14 16:35:49 +0300

    improve render thread structure

--- src/render.c
+++ src/render.c
@@ -3,14 +3,33 @@
  */
 
 #include <allegro5/allegro.h>
+#include <stdbool.h>
 
 #include "render.h"
 
-void *render_main(ALLEGRO_THREAD *thread, void *arg) {
-    // render loop
+static bool render_setup() {
+    // TODO
+    return true;
+}
+
+static void render_loop(ALLEGRO_THREAD *thread) {
     while (!al_get_thread_should_stop(thread)) {
         // TODO
     }
+}
+
+void *render_main(ALLEGRO_THREAD *thread, void *arg) {
+    bool ret;
+
+    // setup stuff
+    ret = render_setup();
+
+    // exit on failure
+    if (!ret)
+        return NULL;
+
+    // render loop
+    render_loop(thread);
 
     return NULL;
 }
