901c9cd9435dea51e243e0ca2a75999ac9eb634f
royeldar <royeldar@hotmail.com>
2025-05-30 21:08:36 +0300

    improve appimage build script

--- build.sh
+++ build.sh
@@ -4,26 +4,55 @@ set -e
 
 meson build
 meson compile -C build
+
+SHARED_LIBS=
+SHARED_LIBS="$SHARED_LIBS /usr/lib/libc.so.6"
+SHARED_LIBS="$SHARED_LIBS /usr/lib/libm.so.6"
+SHARED_LIBS="$SHARED_LIBS /usr/lib/libpthread.so.0"
+SHARED_LIBS="$SHARED_LIBS /usr/lib/librt.so.1"
+SHARED_LIBS="$SHARED_LIBS /usr/lib/libstdc++.so.6"
+SHARED_LIBS="$SHARED_LIBS /usr/lib/libz.so.1"
+SHARED_LIBS="$SHARED_LIBS /usr/lib/libfontconfig.so.1"
+SHARED_LIBS="$SHARED_LIBS /usr/lib/libfreetype.so.6"
+SHARED_LIBS="$SHARED_LIBS /usr/lib/libfribidi.so.0"
+SHARED_LIBS="$SHARED_LIBS /usr/lib/libharfbuzz.so.0"
+
+EXCLUDE_LIBS=
+EXCLUDE_LIBS="$EXCLUDE_LIBS libwayland-*"
+EXCLUDE_LIBS="$EXCLUDE_LIBS libgdk-3.so.0"
+EXCLUDE_LIBS="$EXCLUDE_LIBS libgtk-3.so.0"
+
+EXECUTABLE="build/game"
+DESKTOP="misc/game.desktop"
+ICON="misc/game.svg"
+APPRUN="misc/AppRun"
+
+INTERP="/lib64/ld-linux-x86-64.so.2"
+
+APPIMAGE="game-x86_64.AppImage"
+
+EXTRA_LIBS=""
+
+for lib in $SHARED_LIBS; do
+    EXTRA_LIBS="$EXTRA_LIBS --library $lib"
+done
+
+for lib in $EXCLUDE_LIBS; do
+    EXTRA_LIBS="$EXTRA_LIBS --exclude-library $lib"
+done
+
+APPDIR="AppDir"
+
 ./linuxdeploy-x86_64.AppImage \
-    --appdir AppDir \
-    --library /usr/lib/libc.so.6 \
-    --library /usr/lib/libm.so.6 \
-    --library /usr/lib/libpthread.so.0 \
-    --library /usr/lib/librt.so.1 \
-    --library /usr/lib/libstdc++.so.6 \
-    --library /usr/lib/libz.so.1 \
-    --library /usr/lib/libfontconfig.so.1 \
-    --library /usr/lib/libfreetype.so.6 \
-    --library /usr/lib/libfribidi.so.0 \
-    --library /usr/lib/libharfbuzz.so.0 \
-    --exclude-library libwayland-* \
-    --exclude-library libgdk-3.so.0 \
-    --exclude-library libgtk-3.so.0 \
-    --executable build/game \
-    --desktop-file misc/game.desktop \
-    --icon-file misc/game.svg \
-    --custom-apprun misc/AppRun
-mkdir AppDir/lib64
-cp /lib64/ld-linux-x86-64.so.2 AppDir/lib64
-ARCH=x86_64 ./appimagetool-x86_64.AppImage AppDir
-mv game-x86_64.AppImage bin/
+    --appdir "$APPDIR" \
+    $EXTRA_LIBS \
+    --executable "$EXECUTABLE" \
+    --desktop-file "$DESKTOP" \
+    --icon-file "$ICON" \
+    --custom-apprun "$APPRUN"
+
+mkdir -p "$APPDIR$(dirname "$INTERP")"
+cp "$INTERP" "$APPDIR$INTERP"
+
+ARCH=x86_64 ./appimagetool-x86_64.AppImage "$APPDIR"
+mv "$APPIMAGE" bin/
