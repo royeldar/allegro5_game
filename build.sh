4560c139a087faf2a063a74ec60d90195e0698ec
royeldar <royeldar@hotmail.com>
2025-05-30 20:10:51 +0300

    bundle some more libraries in appimage

--- build.sh
+++ build.sh
@@ -12,6 +12,10 @@ meson compile -C build
     --library /usr/lib/librt.so.1 \
     --library /usr/lib/libstdc++.so.6 \
     --library /usr/lib/libz.so.1 \
+    --library /usr/lib/libfontconfig.so.1 \
+    --library /usr/lib/libfreetype.so.6 \
+    --library /usr/lib/libfribidi.so.0 \
+    --library /usr/lib/libharfbuzz.so.0 \
     --executable build/game \
     --desktop-file misc/game.desktop \
     --icon-file misc/game.svg \
