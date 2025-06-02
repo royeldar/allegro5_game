5224509213905a01daaf41b5a311ac7af107cbf4
royeldar <royeldar@hotmail.com>
2025-06-02 16:13:00 +0300

    add gfx resources directory to appimage and zip

--- build-win32.sh
+++ build-win32.sh
@@ -6,10 +6,12 @@ meson --cross-file win32/x86_64-w64-mingw32.txt build-win32
 meson compile -C build-win32
 
 EXECUTABLE="build-win32/game.exe"
+GFX_DIR="misc/gfx"
 ZIP="bin-win32/game.zip"
 
 mkdir tmp
 cp "$EXECUTABLE" tmp
+cp -r "$GFX_DIR" tmp
 
 cd tmp
 zip -r ../"$ZIP" .
